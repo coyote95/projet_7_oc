d144de7b86151e081af346b457bf82185228f1af
coyote95 <marie.guillot.oc@gmail.com>
2023-12-07 16:57:51 +0100

    forcebrut clean2

--- forcebrut.py
+++ forcebrut.py
@@ -9,7 +9,7 @@ def open_csv_file(file_name):
             csv_content = [{key.strip('\ufeff'): value.strip() for key, value in row.items()} for row in csv_content]
             for row in csv_content:
                 row['cout'] = int(row['cout'])
-                row['benefice'] = float(row['benefice'].rstrip('%'))/100
+                row['benefice'] = float(row['benefice'].rstrip('%')) / 100
         return csv_content
     except FileNotFoundError:
         print(f"The file {file_name} was not found.")
@@ -19,19 +19,10 @@ def open_csv_file(file_name):
         return None
 
 
-csv_file_name = "actions.csv"
-list_actions = open_csv_file(csv_file_name)
-print(list_actions)
-
-all_combinaisons = []
-all_combinaisons_plafond = []
-
-
 def generate_combinations(base_combinaison, remaining_actions, max_depth, result):
     if len(base_combinaison) == max_depth:
         result.append(base_combinaison)
         return
-
     for action in remaining_actions:
         if len(base_combinaison) > 0 and action['actions'] <= base_combinaison[-1]['actions']:
             continue
@@ -39,35 +30,34 @@ def generate_combinations(base_combinaison, remaining_actions, max_depth, result
         generate_combinations(base_combinaison + [action], new_remaining_actions, max_depth, result)
 
 
+def somme_combinations(all_combinations, plafond):
+    for combination in all_combinations:
+        total_cout = sum([action['cout'] for action in combination])
+        if total_cout < plafond:
+            total_benefice = sum([(action['benefice'] * action['cout']) for action in combination])
+            dict_somme_combination = {'actions': [action['actions'] for action in combination],
+                                      'total_cout': total_cout, 'total_benefice': total_benefice}
+            all_combinaison_limit_profit.append(dict_somme_combination)
+    return all_combinaison_limit_profit
+
+
+all_combinaisons = []
+all_combinaison_limit_profit = []
+
+csv_file_name = "actions.csv"
+list_actions = open_csv_file(csv_file_name)
+print(list_actions)
+
 for i in range(1, len(list_actions) + 1):
     print(f"depth={i}")
     generate_combinations([], list_actions, i, all_combinaisons)
 
-
-def somme_combinations(all_combinaisons, plafond):
-    for combination in all_combinaisons:
-        action_names = [action['actions'] for action in combination]
-        total_cout = sum([action['cout'] for action in combination])
-        if total_cout < plafond:
-
-            total_benefice = sum([((action['benefice']/100) * action['cout'] ) for action in combination])
-            result_combination = {'actions': [action['actions'] for action in combination], 'total_cout': total_cout,
-                                  'total_benefice': total_benefice}
-
-            all_combinaisons_plafond.append(result_combination)
-            # print(f"Combinaison : {action_names}, Somme : {total_cout}, Benefice: {total_benefice}")
-
-    return all_combinaisons_plafond
-
-
 resultat = somme_combinations(all_combinaisons, 500)
-resultat_trie = sorted(resultat, key=lambda x: x['total_benefice'], reverse=False)
-
-# Affichage du résultat trié
-print("Résultat trié par plus grand total bénéfice :")
-for result in resultat_trie:
-    print(f"Combinaison : {result['actions']}, Somme : {result['total_cout']}, Benefice : {result['total_benefice']}")
+resultat.sort(key=lambda x: x['total_benefice'], reverse=True)
 
 
-
-
+best_combination = resultat[0]
+print()
+print(f"la meilleur combinaison est: {best_combination['actions']}")
+print(f"Total cout: {best_combination['total_cout']}")
+print(f"Total benefice: {best_combination['total_benefice']}")
